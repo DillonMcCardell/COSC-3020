49810f8cea84c3cc529e8f4f94e0f42739d9c868
DillonMcC <dmccarde@uwyo.edu>
2021-09-11 19:18:04 -0600

    Added comments

--- HW1_Prob4.js
+++ HW1_Prob4.js
@@ -1,29 +1,31 @@
 // COSC 3020
 // Dillon McCardell and Erin Stanfill
 // Homework 1 Problem 4
-// Merge Sort Iterative and In-place
+// Merge Sort Iterative (current NOT in-place)
+
+/* The merge function merges two sorted parts 
+and returns the merged sorted array */
 function merge(array,L1,R1,L2,R2){
-    var temp = [];
-    var i = 0; 
-    while(L1 <= R1 & L2 <= R2){
-        if(array[L1]<=array[L2]){
-            temp[i] = array[L1];
+    var temp = [];  // Temp array. Must be removed to become in-place
+    var i = 0;      // index to 0          
+    while(L1 <= R1 & L2 <= R2){     // While the bounds are correctly positioned.
+        if(array[L1]<=array[L2]){   // If the leftmost element of array 1 is smaller.
+            temp[i] = array[L1];    // Store lowest value (at L1) in the temp array.        
             i++;
-            L1++;
+            L1++;                   // Increment 1st array's left bound.
         }
-        else{
-            temp[i]=array[L2];
+        else{                       // Else the leftmost element of array 2 is smaller.
+            temp[i]=array[L2];      // Store lowest value (at L2) in the temp array.
             i++;
-            L2++;
+            L2++;                   // Increment 2nd array's left bound.
         }
-        //console.log(temp);
     }
-    while(L1 <= R1){
+    while(L1 <= R1){                // Add remaining elements from first array to temp.
         temp[i] = array[L1];
         i++;
         L1++;
     }
-    while(L2 <= R2){
+    while(L2 <= R2){                // Add remaining elements from second array to temp.
         temp[i] = array[L2];
         i++;
         L2++;
@@ -31,38 +33,47 @@ function merge(array,L1,R1,L2,R2){
     return temp;
 }
 
+/* Implementation of the iterative merge sort function */
 function mergeSort(array,l){
-    var len = 1;
-    var i, j, L1, L2, R1, R2 = 0;
-    while(len < l){
+    var len = 1;                    // len = length of array we will examine.
+    var i, j, L1, L2, R1, R2 = 0;   // Instantiate Variables.
+    while(len < l){                 // While the length of array we examine 
+                                    // is within the bounds of the array we are given.
         i = 0;
-        while(i < l){
-            L1 = i;
-            R1 = i + len - 1;
-            L2 = i + len;
-            R2 = i + (2*len) - 1;
-            if(L2 >= l){
-                console.log("error");
+        while(i < l){               // Stay within bounds of input array length.
+            L1 = i;                 // Left bound of first array = index
+            R1 = i + len - 1;       // Right bound of first array.
+                                    // As len increases, the size of the array
+                                    // increases.
+            L2 = i + len;           // Left bound of the second array.
+            R2 = i + (2*len) - 1;   // Right bound of the second array.
+                                    // Multiply len*2 to match len increase calculation below.
+            if(L2 >= l){            // This handles odd length input arrays.
                 break;
             }
-            if(R2 >= l){
+            if(R2 >= l){            // Adjust size so it doesnt exceed l (remain in bounds).
                 R2 = l-1;
             }
-            console.log(L1, R1, L2, R2);
-            console.log("i: ",i);
-            console.log("len: ",len);
-            var temp = merge(array,L1,R1,L2,R2);
-            for(j = 0; j <= R2-L1; j++){
+            var temp = merge(array,L1,R1,L2,R2);    // Send bounds of arrays to be merged.
+            for(j = 0; j <= R2-L1; j++){            // Copy temp into array
                 array[i+j]=temp[j];
-                console.log("new array: ",array);
             }
-            i = i + 2*len;
+            i = i + 2*len;          // i increases by 2 for the first time throuhg the outer
+                                    // while loop, then 4, then 8. This is due to the fact that
+                                    // we merged two parts of length len, so we know that all 
+                                    // parts of size 2*len are now sorted.
         }
-        len = 2*len;
+        len = 2*len;                // Double len each time through the outer while loop.
     }
     return array;
 }
 
-array = [6,4,2,7,5,3,1,12,52,13,11];
+array = [6,4,2,7,5,3,1,12,52,13,11];    // input array
 
-console.log(mergeSort(array,array.length))
+console.log(mergeSort(array,array.length));
+
+
+// Sources:
+// https://www.baeldung.com/cs/non-recursive-merge-sort // Pseudo code was basis for iterative method
+// https://www.geeksforgeeks.org/iterative-merge-sort/
+// https://stackoverflow.com/questions/58274398/how-merge-sort-works-at-arrays-of-length-n
